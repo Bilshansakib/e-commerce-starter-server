921f0827e3ca2692f49d06c98eae22552c828594
Bilshansakib <burakaghani@gmail.com>
2024-08-17 02:29:16 +0600

    Update: paginationProduct/backend part

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express')
-const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
 
 require("dotenv").config();
-const app = express()
-const port = process.env.PORT || 9000
+const app = express();
+const port = process.env.PORT || 9000;
 
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7hlvjai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,45 +17,78 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const productCollection = client.db('e-commerce-starter').collection('products')
-    app.get('/api/products', async (req, res) => {
-      const {search, category,brand, minPrice, maxPrice, sort} = req.query
-      let query = {}
-        if (search) {
-            query.productName = {$regex: search, $options: 'i'}
+    const productCollection = client
+      .db("e-commerce-starter")
+      .collection("products");
+    // fetch all products
+    app.get("/api/products", async (req, res) => {
+      const { search, category, brand, minPrice, maxPrice, sort } = req.query;
+      let query = {};
+      if (search) {
+        query.productName = { $regex: search, $options: "i" };
+      }
+      if (category) {
+        query.category = category;
+      }
+      if (brand) {
+        query.brandName = brand;
+      }
+      if (minPrice || maxPrice) {
+        query.price = {};
+        if (minPrice) {
+          query.price.$gte = parseFloat(minPrice);
         }
-        if (category){
-            query.category = category
+        if (maxPrice) {
+          query.price.$lte = parseFloat(maxPrice);
         }
-        if (brand){
-            query.brandName = brand
-        }
-        if (minPrice || maxPrice) {
-            query.price ={}
-            if( minPrice) {
-                query.price.$gte = parseFloat(minPrice)
-            }
-            if( maxPrice) {
-                query.price.$lte = parseFloat(maxPrice)
-            }
-        }
-        let products =await productCollection.find(query).toArray()
-        
-        if (sort) {
-          const sortOrder = sort === 'asc' ? 1 : -1;
-          products = products.sort((a,b) => (a.price - b.price) * sortOrder);
-        }
-        res.send(products)
-    })
+      }
+      let products = await productCollection.find(query).toArray();
+
+      if (sort) {
+        const sortOrder = sort === "asc" ? 1 : -1;
+        products = products.sort((a, b) => (a.price - b.price) * sortOrder);
+      }
+      res.send(products);
+    });
+    // pagination
+    app.get("/paginatedProducts", async (req, res) => {
+      const allProduct = await productCollection.find().toArray();
+      const page = parseInt(req.query.page)
+      const limit = parseInt(req.query.limit)
+
+      const startIndex = (page - 1) * limit;
+      const lastIndex = page * limit;
+      const results = {};
+      results.totalProducts =allProduct.length
+      results.pageCount = Math.ceil(allProduct.length/limit)
+
+      if (lastIndex < allProduct.length) {
+        results.next = {
+          page: page + 1,
+        };
+      }
+
+      if (startIndex > 0) {
+        results.prev = {
+          page: page - 1,
+        };
+      }
+
+      results.result = allProduct.slice(startIndex, lastIndex);
+
+      res.json(results);
+    });
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -66,9 +96,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("Hello from saaqib server....");
-  });
+  res.send("Hello from saaqib server....");
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
