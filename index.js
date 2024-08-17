6c1d78216c033ad17e52c693f414fb1f89f5ccaf
Bilshansakib <burakaghani@gmail.com>
2024-08-17 19:56:01 +0600

    Update: cors solution

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:9000",
+    "https://e-commerce-starter-15646.web.app"
+  ],
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7hlvjai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
